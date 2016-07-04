58f1a40c9d35d7ccc5e20d2afc72a834139629a7
Yahia Mansouri <alpatrose@gmail.com>
2016-07-04 03:51:34 +0100

    Some more code

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,26 @@
+defmodule Issues.GithubIssues do
+
+  @user_agent [{"user-agent", "Elixir Y"}]
+
+ def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_reponse
+ end
+
+ def handle_reponse({:ok, %{status_code: 200, body: body}}) do
+   {:ok, Poison.Parser.parse!(body)}
+ end
+
+ def handle_reponse({_, %{status_code: ___, body: body}}) do
+   {:error, Poison.Parser.parse!(body)}
+ end
+
+ # use a module attribute to fetch the value at compile time
+ @github_url Application.get_env(:issues, :github_url)
+
+ def issues_url(yser, project) do
+   "#{@github_url}/repos/#{user}/#{project}/issues"
+ end
+
+end
