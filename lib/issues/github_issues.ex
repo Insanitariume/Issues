ff128a609494f4a19b1ef4b5dae9787c590cb384
Yahia Mansouri <alpatrose@gmail.com>
2016-07-07 21:41:50 +0100

    Add DocTest

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,18 +1,24 @@
 defmodule Issues.GithubIssues do
 
+  require Logger
+
   @user_agent [{"user-agent", "Elixir Y"}]
 
- def fetch(user, project) do
+  def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}'"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_reponse
  end
 
- def handle_reponse({:ok, %{status_code: 200, body: body}}) do
+  def handle_reponse({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful reponse"
+    Logger.debug fn -> inspect(body) end
    {:ok, Poison.Parser.parse!(body)}
  end
 
- def handle_reponse({_, %{status_code: ___, body: body}}) do
+  def handle_reponse({_, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
    {:error, Poison.Parser.parse!(body)}
  end
 
